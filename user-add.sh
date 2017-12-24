313531b7373ad7bde8c9a9d0e986489a5f0ea59d
griffin8828 <32222465+griffin8828@users.noreply.github.com>
2017-12-24 21:10:43 +0800

    Update user-add.sh

--- user-add.sh
+++ user-add.sh
@@ -38,27 +38,27 @@ echo -e ""
 echo -e ""
 echo -e ""
 echo -e "==========================================="
-echo -e "|         Informasi Akun Baru SSH         |"  | lolcat
+echo -e "|         Informasi Akun Baru SSH         |"  
 echo -e "============[[-SERVER-PREMIUM-]]===========" 
-echo -e "     Host: $MYIP"  | lolcat
-echo -e "     Username: $username"  | lolcat
-echo -e "     Password: $password                   " | lolcat
-echo -e "     Port default dropbear: 442,80         "  | lolcat
-echo -e "     Port default SSL/TLS : 443            " | lolcat
-echo -e "     Port default openSSH : 22,143         "  | lolcat
-echo -e "     Port default squid   : 8080,3128      "  | lolcat
+echo -e "     Host: $MYIP"  
+echo -e "     Username: $username"  
+echo -e "     Password: $password                   " 
+echo -e "     Port default dropbear: 777,80         "  
+echo -e "     Port default SSL/TLS : 443            " 
+echo -e "     Port default openSSH : 22,143         "  
+echo -e "     Port default squid   : 8080,3128      "  
 echo -e "                                           " 
-echo -e "     Peringatan: Maksimal 2 login          "  | lolcat
+echo -e "     Peringatan: Maksimal 2 login          "  
 echo -e "-------------------------------------------"
-echo -e "     Aktif Sampai: $(date -d "$AKTIF days" +"%d-%m-%Y")"  | lolcat
+echo -e "     Aktif Sampai: $(date -d "$AKTIF days" +"%d-%m-%Y")"  
 echo -e "===========================================" 
-echo -e "   DI LARANG:                              " | lolcat
-echo -e "   HACKING-DDOS-PHISING-SPAM-TORENT        "  | lolcat
-echo -e "   CARDING-CRIMINAL CYBER.                 " | lolcat
+echo -e "   DI LARANG:                              " 
+echo -e "   HACKING-DDOS-PHISING-SPAM-TORENT        "  
+echo -e "   CARDING-CRIMINAL CYBER.                 " 
 echo -e "===========================================" 
-echo -e "   Script by Yusuf Ardiansyah              " | lolcat
-echo -e "   Config OVPN:                            " | lolcat
-echo -e "   http://$MYIP:81/client.ovpn        "  | lolcat
+echo -e "   Script by Yusuf Ardiansyah              " 
+echo -e "   Config OVPN:                            " 
+echo -e "   http://$MYIP:81/client.ovpn        "  
 echo -e "-------------------------------------------"
 echo -e ""
 echo -e ""
